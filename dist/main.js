d7ed321934cc6c509624fe65d408d5d6ccf96368
ArturoRen <lzm.cyr@gmail.com>
2024-09-09 22:42:14 +0800

    feat: :sparkles: 添加数据验证管道

--- dist/main.js
+++ dist/main.js
@@ -7,19 +7,21 @@ const path_1 = __importDefault(require("path"));
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const fastify_adapter_1 = require("./common/adapters/fastify.adapter");
-const config_1 = require("@nestjs/config");
 const env_1 = require("./global/env");
 const logging_interceptor_1 = require("./common/interceptors/logging.interceptor");
+const class_validator_1 = require("class-validator");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, fastify_adapter_1.fastifyApp, {
         bufferLogs: true,
         snapshot: true,
     });
-    const configService = app.get((config_1.ConfigService));
+    const configService = app.get((ConfigService));
     const { port, globalPrefix } = configService.get('app', { infer: true });
     app.enableCors({ origin: '*', credentials: true });
     app.setGlobalPrefix(globalPrefix);
+    (0, class_validator_1.useContainer)(app.select(app_module_1.AppModule), { fallbackOnErrors: true });
     app.useStaticAssets({ root: path_1.default.join(__dirname, '..', 'public') });
+    app.useGlobalPipes();
     if (env_1.isDev)
         app.useGlobalInterceptors(new logging_interceptor_1.LoggingInterceptor());
     await app.listen(port, '0.0.0.0');
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import path from 'path'
+import cluster from 'node:cluster'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { NestFastifyApplication } from '@nestjs/platform-fastify'
 import { fastifyApp } from './common/adapters/fastify.adapter'
-import { ConfigService } from '@nestjs/config'
 import { ConfigKeyPaths } from './config'
-import cluster from 'node:cluster'
-import { isDev, isMainProcess } from './global/env'
+import { isDev } from './global/env'
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor'
+import { useContainer } from 'class-validator'
+import { NestFastifyApplication } from '@nestjs/platform-fastify'
+import { ConfigService } from '@nestjs/config'
+import { HttpStatus, UnprocessableEntityException, ValidationPipe } from '@nestjs/common'
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastifyApp, {
     bufferLogs: true,
@@ -20,8 +22,31 @@ async function bootstrap() {
   app.enableCors({ origin: '*', credentials: true })
   /// 启用全局前缀
   app.setGlobalPrefix(globalPrefix)
+  /// class-validator (用于自定义验证器)
+  useContainer(app.select(AppModule), { fallbackOnErrors: true })
   /// 静态资源服务
   app.useStaticAssets({ root: path.join(__dirname, '..', 'public') })
+  /// 全局验证管道
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      //是否禁止未在白名单中的属性出现在请求数据
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      transformOptions: { enableImplicitConversion: true },
+      stopAtFirstError: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      exceptionFactory: (errors) =>
+        new UnprocessableEntityException(
+          errors.map((e) => {
+            const rule = Object.keys(e.constraints!)[0]
+            const msg = e.constraints![rule]
+            return msg
+          })[0],
+        ),
+    }),
+  )
   if (isDev) app.useGlobalInterceptors(new LoggingInterceptor())
   await app.listen(port, '0.0.0.0')
 }
