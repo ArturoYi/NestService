78a5764b04cc2c14fd6e56828c13d3d818b96ae2
ArturoRen <lzm.cyr@gmail.com>
2024-09-26 02:15:46 +0800

    feat: :sparkles: 补充数据库内容

--- src/shared/datebase/database.module.ts
+++ src/shared/datebase/database.module.ts
@@ -5,8 +5,10 @@ import { ConfigKeyPaths, IDatabaseConfig } from '@project/config'
 import { env } from '@project/global/env'
 import { DataSource, LoggerOptions } from 'typeorm'
 import { TypeORMLogger } from './typeorm-logger'
+import { EntityExistConstraint } from './constraints/entity-exist.constraint'
+import { UniqueConstraint } from './constraints/unique.constraint'
 
-const providers = []
+const providers = [EntityExistConstraint, UniqueConstraint]
 const logger = new Logger('DatabaseModule')
 
 @Module({
